2b15269f140e53db635e020bdcdc516c6361bcb4
srtasantiago <amanda.santxiago2019@hotmail.com>
2025-07-05 11:21:05 -0300

    refac: removendo projeto em java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import view.TelaUsuario;
-
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new TelaUsuario().setVisible(true);
-        });
-    }
-}
